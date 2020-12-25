f0685710911349d76d74e79d7afc094c47003b0c
Ondrej Sika <ondrej@ondrejsika.com>
2020-12-25 12:19:24 +0100

    Fix package (add __init__.py), VERSION 1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="boolenv",
-    version="1.0.1",
+    version="1.0.4",
     author="Ondrej Sika",
     author_email="ondrej@ondrejsika.com",
     description="Read Boolean value from environment variable",
